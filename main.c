428ecefa1fd553cfafb1632401a714cef7032086
jaylenperez <jaylenestella24@gmail.com>
2024-03-23 10:50:30 -0500

    Completed part 1 of obeds article for our main function and fixed errors

--- main.c
+++ main.c
@@ -6,8 +6,11 @@ int main(int ac, char **argv) // Main function with command-line arguments
     char *lineptr; // Declare a pointer to a character array, which will hold the input line
     size_t n = 0; // Store allocated size in bytes
 
-    printf("%s, prompt"); // Print the prompt to the console
+    (void)ac; (void)argv; // Declaring void variables
+
+    printf("%s", prompt); // Print the prompt to the console
     getline(&lineptr, &n, stdin); // Read a line from the standard input (stdin) into lineptr, dynamically allocating memory as needed
+    printf("%s\n", lineptr); // Printing to make sure getline() worked
 
     free(lineptr); // Free the memory allocated for lineptr to prevent memory leaks
     return (0); // Return 0 to indicate successful execution of the program
