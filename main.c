53056631bb5fdfd9107d050765e1339ef1feed71
jaylenperez <jaylenestella24@gmail.com>
2024-03-25 10:22:19 -0500

    Moved lines to correct tab indentation format

--- main.c
+++ main.c
@@ -13,55 +13,57 @@ int main(int ac, char **argv) // Main function with command-line arguments
 
     (void)ac; // Declaring void variables
 
-        while (1){ // Added a while loop to continue giving us eshell prompt
-            printf("%s", prompt); // Print the prompt to the console
-            nchars_read = getline(&lineptr, &n, stdin); // getting user input with getline and storing the number of chars read in the variable nchars_read
-                if (nchars_read == -1){ // If getline function fails or user types CTRL + D
-                    printf("Exiting shell....\n"); // Print the exit message
-                    return (-1); // Return -1 and exit the program
-                }
-/*allocate space for a copy of the lineptr*/
-    lineptr_copy = malloc(sizeof(char) *nchars_read);
-if (lineptr_copy == NULL){
-    perror("tsh: memory allocation error");
-
-    return(-1);
-}
-
-    /*copy lineptr to lineptr_copy*/
-    strcpy(lineptr_copy, lineptr);
-
-    /* slit the string (lineptr) into an array of words*/
-    /* calculate the total number of tokens*/
-    token = strtok(lineptr, delim);
-
-    while (token != NULL)
+    while (1) // Added a while loop to continue giving us eshell prompt
     {
+        printf("%s", prompt); // Print the prompt to the console
+        nchars_read = getline(&lineptr, &n, stdin); // getting user input with getline and storing the number of chars read in the variable nchars_read
+        if (nchars_read == -1) // If getline function fails or user types CTRL + D
+        {
+            printf("Exiting shell....\n"); // Print the exit message
+            return (-1); // Return -1 and exit the program
+        }
+        // allocate space for a copy of the lineptr*/
+        lineptr_copy = malloc(sizeof(char) *nchars_read);
+        if (lineptr_copy == NULL)
+        {
+            perror("tsh: memory allocation error");
+
+            return(-1);
+        }
+        //copy lineptr to lineptr_copy*/
+        strcpy(lineptr_copy, lineptr);
+
+        // slit the string (lineptr) into an array of words*/
+        // calculate the total number of tokens*/
+        token = strtok(lineptr, delim);
+
+        while (token != NULL)
+        {
+            num_tokens++;
+            token = strtok(NULL, delim);
+        }
         num_tokens++;
-        token = strtok(NULL, delim);
-    }
-    num_tokens++;
 
-    /* allocae space to hold the array of strings */
-     argv = malloc(sizeof(char *) * num_tokens);
+        // allocae space to hold the array of strings */
+        argv = malloc(sizeof(char *) * num_tokens);
 
-     /* store each token in the argv array */
-     token = strtok(lineptr_copy, delim);
+        // store each token in the argv array */
+        token = strtok(lineptr_copy, delim);
      
-    for(i = 0; token != NULL; i++)
-    {
-        argv[i] = malloc(sizeof(char) * strlen(token));
-        strcpy(argv[i], token);
+        for(i = 0; token != NULL; i++)
+        {
+            argv[i] = malloc(sizeof(char) * strlen(token));
+            strcpy(argv[i], token);
  
-        token = strtok(NULL, delim); 
-    }
-    argv[i] = NULL;         
+            token = strtok(NULL, delim); 
+        }
+        argv[i] = NULL;
 
 
-    printf("%s\n", lineptr); // Printing to make sure getline() worked
+        printf("%s\n", lineptr); // Printing to make sure getline() worked
 
-    free(lineptr); // Free the memory allocated for lineptr to prevent memory leaks
-    lineptr = NULL; // Reset lineptr to NULL to avoid dangling pointer
+        free(lineptr); // Free the memory allocated for lineptr to prevent memory leaks
+        lineptr = NULL; // Reset lineptr to NULL to avoid dangling pointer
     }
 
     return (0); // Return 0 to indicate successful execution of the program
