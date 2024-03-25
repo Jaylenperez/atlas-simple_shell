7844eac9ad743d004afad502397e1a27d360bd0e
jaylenperez <jaylenestella24@gmail.com>
2024-03-25 11:50:05 -0500

    Created a function to execute the commands

--- execmd.c
+++ execmd.c
@@ -2,5 +2,17 @@
 
 void execmd(char **argv)
 {
-    
+    char *command = NULL; // Declare a pointer to a character (string) and initialize it to NULL
+
+    if (argv) // Check if the argv pointer is not NULL
+    {
+        /* Get the command */
+        command = argv[0]; // Assign the first element of argv (command name) to the command pointer
+
+        /* Execute the command with execve */
+        if (execve(command, argv, NULL) == -1) // Attempt to execute the command using execve. If execve returns -1, it indicates an error
+        {
+            perror("Error:"); // Print an error message to stderr, prefixed with "Error:"
+        }
+    }
 }
