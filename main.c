d48959529811e2c668f0e42363ce5f86ffaf6dc0
jaylenperez <jaylenestella24@gmail.com>
2024-03-29 10:47:39 -0500

    commented out exit shell message

--- main.c
+++ main.c
@@ -20,11 +20,11 @@ int main(int ac, char **argv)
         {
             printf("%s", prompt);
         }
-        
+
         nchars_read = getline(&lineptr, &n, stdin);
         if (nchars_read == -1)
         {
-            printf("Exiting shell....\n");
+            // printf("Exiting shell....\n");
             break;
         }
         lineptr_copy = malloc(sizeof(char) * (nchars_read + 1));
