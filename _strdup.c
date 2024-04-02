539f6ff6bc9b452d723018c0586847a1c89d3168
jaylenperez <jaylenestella24@gmail.com>
2024-04-01 22:04:08 -0500

    correct betty documentation

--- _strdup.c
+++ _strdup.c
@@ -1,8 +1,12 @@
 #include "main.h"
+
 /**
  * _strdup - Duplicate a string.
  * @str: The string to duplicate.
  *
+ * Description: _strdup creates a new string in memory which is a duplicate
+ * of the string passed as parameter.
+ * 
  * Return: A pointer to the duplicated string, or NULL if memory alloc fails.
  */
 char *_strdup(const char *str)
