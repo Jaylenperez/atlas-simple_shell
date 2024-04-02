41c8b3cb3ac1f922d1f5b4e784755faba32d888b
jaylenperez <jaylenestella24@gmail.com>
2024-04-01 20:34:27 -0500

    converted indentation to tabs in _getenv.c

--- _getenv.c
+++ _getenv.c
@@ -7,17 +7,17 @@
 
 char *_getenv(const char *name)
 {
-    extern char **environ;
-    char **env;
-    size_t name_len;
-    name_len = strlen(name);
+	extern char **environ;
+	char **env;
+	size_t name_len;
+	name_len = strlen(name);
 
-    for (env = environ; *env != NULL; env++)
-    {
-        if (strncmp(*env, name, name_len) == 0 && (*env)[name_len] == '=')
-        {
-            return (*env + name_len + 1);
-        }
-    }
-    return (NULL);
+	for (env = environ; *env != NULL; env++)
+	{
+		if (strncmp(*env, name, name_len) == 0 && (*env)[name_len] == '=')
+		{
+			return (*env + name_len + 1);
+		}
+	}
+	return (NULL);
 }
